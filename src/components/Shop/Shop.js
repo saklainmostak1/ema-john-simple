bc23c2e6b5bede12743eefb26898cead9c6e3bf3
saklainMostake <saklain15-12003@diu.edu.bd>
2022-09-26 03:54:26 +0600

    push

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb'
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -15,11 +16,20 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, [])
 
+    useEffect(() =>{
+        const storedCart = getStoredCart()
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id == id)
+            console.log(addedProduct)
+        }
+    }, [])
+
     const handleAddToCart = (product) =>{
         console.log(product)
         // cart.push(product)
         const newCart = [...cart, product]
         setCart(newCart)
+        addToDb(product.id)
     }
 
     return (
